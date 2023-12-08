1bb4668cdc1cba5522a5924c3cf5e0d628bfbf1f
Alpsriva <57250701+Alpsriva@users.noreply.github.com>
2023-12-08 12:01:02 +0530

    Add files via upload

---
+++ element.js
@@ -0,0 +1,8 @@
+import '../src/x-801052-example-rest';
+
+const el = document.createElement('DIV');
+document.body.appendChild(el);
+
+el.innerHTML = `		
+<x-801052-example-rest></x-801052-example-rest>
+`;
